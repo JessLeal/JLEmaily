b82dce680483da1d7b74d0ee69dbe8c740151c09
JessLeal <lealjess2158140@gmail.com>
2021-01-30 11:10:38 +0800

    Fixed proxy issue by adding configuration to GoogleStart

--- config/prod.js
+++ config/prod.js
@@ -5,6 +5,3 @@ module.exports = {
   cookieKey: process.env.COOKIE_KEY,
 };
 
-// mongodb+srv://dbProd:asBaAOFYvgJbKUFf@cluster0.rf4gj.mongodb.net/dbProd?retryWrites=true&w=majority
-//65246885463-u82s3r6550gr0dpo2svsnpc6svrffsg0.apps.googleusercontent.com - client id
-//5IfTK5cheYFEyhrkszNb2g1p client secret
