e122ca80e940dc8dd8a2cde9c618e7298b23faef
JessLeal <lealjess2158140@gmail.com>
2021-02-01 23:34:29 +0800

    fix surveys bug

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -34,7 +34,7 @@ class Header extends Component {
       <nav>
         <div className="nav-wrapper">
           <Link
-            to={this.props.auth ? './surveys' : '/'}
+            to={this.props.auth ? '/surveys' : '/'}
             className="left brand-logo"
           >
             Emaily
